d7ee4464d79f6b65fafe09231a9d98087e0f2f01
Charles Fleury <chs.fleury@gmail.com>
2022-02-14 23:53:38 +0100

    add some stats

--- src/main/kotlin/fr/chsfleury/cryptomoon/domain/model/stats/PortfolioStats.kt
+++ src/main/kotlin/fr/chsfleury/cryptomoon/domain/model/stats/PortfolioStats.kt
@@ -1,10 +1,25 @@
 package fr.chsfleury.cryptomoon.domain.model.stats
 
+import fr.chsfleury.cryptomoon.domain.model.Fiat.USD
 import fr.chsfleury.cryptomoon.utils.FiatMap
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class PortfolioStats (
     val name: String,
     val total: FiatMap,
     val athTotal: FiatMap,
     val accountStats: Set<AccountStats>,
-)
+) {
+    val ratioAth: BigDecimal = athTotal[USD]
+        ?.to(total[USD] ?: BigDecimal.ZERO)
+        ?.let { it.second.multiply(BigDecimal(100)) / it.first }
+        ?.setScale(2, RoundingMode.HALF_UP)
+        ?: BigDecimal.ZERO
+
+    val athMultiplier: BigDecimal = total[USD]
+        ?.to(athTotal[USD] ?: BigDecimal.ZERO)
+        ?.let { it.second / it.first }
+        ?.setScale(1, RoundingMode.HALF_UP)
+        ?: BigDecimal.ZERO
+}
