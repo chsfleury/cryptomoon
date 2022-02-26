918db10b45990ab26ec75cbba697ab016adc21e0
Charles Fleury <chs.fleury@gmail.com>
2022-02-26 23:42:29 +0100

    improve trigger service

--- src/main/kotlin/fr/chsfleury/cryptomoon/domain/service/TriggerService.kt
+++ src/main/kotlin/fr/chsfleury/cryptomoon/domain/service/TriggerService.kt
@@ -2,11 +2,11 @@ package fr.chsfleury.cryptomoon.domain.service
 
 import fr.chsfleury.cryptomoon.domain.repository.TriggerRepository
 import fr.chsfleury.cryptomoon.domain.trigger.Trigger
-import fr.chsfleury.cryptomoon.utils.GCD
 import fr.chsfleury.cryptomoon.utils.Logging
 import fr.chsfleury.cryptomoon.utils.logger
 import java.time.Duration
 import java.time.Instant
+import java.time.temporal.ChronoUnit
 import java.util.*
 import kotlin.concurrent.timer
 
@@ -16,20 +16,22 @@ class TriggerService(
 ): Logging {
     private val log = logger()
     private val triggers: List<Trigger>
-    private val minDelay: Duration
     private lateinit var timer: Timer
+
+    private val lastTriggeredCache = mutableMapOf<String, Instant>()
+    private var lastTriggeredCacheExpiration: Instant = Instant.now()
 
     init {
         triggers = triggerArgs.toList()
-        val minDelayInSeconds = GCD.compute(triggers.map { it.delay.seconds.toInt() })
-        minDelay = Duration.ofSeconds(minDelayInSeconds.toLong())
-        log.info("trigger loop set to {} minutes", minDelay.toMinutes())
     }
 
     fun start(): TriggerService {
-        timer = timer("main", true, 0L, minDelay.toMillis()) {
+        timer = timer("main", true, 0L, Duration.ofMinutes(2L).toMillis()) {
+            log.debug("check triggers")
             val executed = check()
-            log.info("executed: {}", executed)
+            if (executed.isNotEmpty()) {
+                log.info("executed: {}", executed)
+            }
         }
         return this
     }
@@ -40,7 +42,7 @@ class TriggerService(
 
     fun check(force: Boolean = false): Set<String> {
         val now = Instant.now()
-        val triggeredAt = triggerRepository.lastTriggered()
+        val triggeredAt = lastTriggered(now)
         val executedSet = mutableSetOf<String>()
         val triggerExecuted = triggers.asSequence()
             .filter { it.trigger(triggeredAt, now, force) }
@@ -49,13 +51,33 @@ class TriggerService(
             .partition { it in triggeredAt.keys }
 
         if (triggerExecuted.first.isNotEmpty()) {
+            triggerExecuted.first.forEach { triggerName ->
+                lastTriggeredCache[triggerName] = now
+            }
             triggerRepository.update(triggerExecuted.first, now)
         }
 
         if (triggerExecuted.second.isNotEmpty()) {
+            triggerExecuted.second.forEach { triggerName ->
+                lastTriggeredCache[triggerName] = now
+            }
             triggerRepository.insert(triggerExecuted.second, now)
         }
 
         return executedSet
+    }
+
+    private fun lastTriggered(now: Instant): Map<String, Instant> {
+        return if (now < lastTriggeredCacheExpiration) {
+            if (lastTriggeredCache.isEmpty()) {
+                lastTriggeredCache += triggerRepository.lastTriggered()
+            }
+            lastTriggeredCache
+        } else {
+            lastTriggeredCache.clear()
+            lastTriggeredCache += triggerRepository.lastTriggered()
+            lastTriggeredCacheExpiration = now.plus(15L, ChronoUnit.MINUTES)
+            lastTriggeredCache
+        }
     }
 }
